d21c45231d5c31ce85ecd57dfa4f11b289bf42ef
Leandro <Leandro>
2020-10-16 21:18:47 -0300

    Objeto pokemons não é mais passado via props.

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -4,16 +4,18 @@ class Pokemon extends React.Component {
     render() {
         const { name, type, averageWeight, image } = this.props.pokemon;
 
-        return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-                </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
-        );
+      return (
+        <div className="pokemon">
+          <div>
+            <p> {name} </p>
+            <p> {type} </p>
+            <p> {`Average weight: ${averageWeight.value}
+              ${averageWeight.measurementUnit}`}
+            </p>
+          </div>
+          <img src={image} alt={`${name} sprite`} />
+        </div>
+      );
     }
 }
 
