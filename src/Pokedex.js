925e093db0f7a9c250157f993616300848349ff0
Leandro <Leandro>
2020-10-16 21:17:51 -0300

    Feito uso do state do componente para renderização dos cards pokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import pokemons from './data'; 
 
 class Pokedex extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      pokemon: pokemons[0],
+      counter: 0,
+    }
+    this.savePokemonState = this.savePokemonState.bind(this)
+  }
+
+  getPokemons() {
+    this.setState({ pokemons: pokemons })
+  }
+
+  savePokemonState() {
+    if (this.state.counter == 8) {
+      this.setState({ pokemon: this.state.pokemons[0] })
+    }
+    this.setState((previousState, _props) => ({
+      counter: previousState.counter + 1,
+      pokemon: this.state.pokemons[this.state.counter],
+    }))
+    console.log(this.state.counter)
+  }
+
+
+
+  componentDidMount() {
+    this.getPokemons()
+  }
+
     render() {
+      const { pokemon } = this.state
         return (
+          <div>
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+              <Pokemon pokemon={pokemon} />)
             </div>
+            <input
+              type="button"
+              value="Next"
+              onClick={this.savePokemonState}
+            />
+          </div>
         );
     }
 }
