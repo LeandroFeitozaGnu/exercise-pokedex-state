1882d6ee1ce59e41ae98ef9e9346d009449aefb7
Leandro Feitoza <leandro@fedora.localdomain>
2020-12-21 17:30:52 -0300

    Updates

---
+++ precache-manifest.394435ff6549117e8ba30bc57581b577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69790779b6caa117629c3e072bf9d782",
+    "url": "/exercise-pokedex-state/index.html"
+  },
+  {
+    "revision": "046560d41107c61975bd",
+    "url": "/exercise-pokedex-state/static/css/main.08ea3c56.chunk.css"
+  },
+  {
+    "revision": "2448bc44a6e7b1af2127",
+    "url": "/exercise-pokedex-state/static/js/2.818e2a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise-pokedex-state/static/js/2.818e2a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046560d41107c61975bd",
+    "url": "/exercise-pokedex-state/static/js/main.953e4bfb.chunk.js"
+  },
+  {
+    "revision": "c79fe6c743eb74274405",
+    "url": "/exercise-pokedex-state/static/js/runtime-main.9497bcf2.js"
+  }
+]);
